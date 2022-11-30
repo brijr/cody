21d85b8c584f312a97e71bdb0a70e58aaaf35176
Bridger Tower <you@example.com>
2022-11-30 15:10:09 -0700

    Updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Sidebar from '../components/sidebar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className='p-4 lg:p-12'>
+    <main>
+      <Sidebar />
       <Component {...pageProps} />
     </main>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,33 @@
 import Head from "next/head";
 import Image from "next/image";
+// Component Imports
+// import Sidebar from "../components/sidebar";
 // Image Imports
-import cody from "../public/cody.jpeg"
+import cody from "../public/cody.jpeg";
 
 export default function Home() {
   return (
-    <main className=" max-w-screen-md">
+    <main className="max-w-screen-sm">
       <Head>
         <title>
           Cody Tower | Champion of Great Places to Work & Great Places to Live
         </title>
       </Head>
-      <div>
-        <Image src={cody} className="mb-12 w-32" alt="Photo of Cody Tower"></Image>
+
+      <div className="p-8 lg:p-24">
+        <Image
+          src={cody}
+          className="mb-12 w-32"
+          alt="Photo of Cody Tower"
+        ></Image>
         <h1 className="text-4xl md:text-6xl italic mb-6">Cody Tower</h1>
         <h3 className="text-xl mb-6">
           Champion of Great Places to Work & Great Places to Live
         </h3>
-        <p className="font-sans">
+        <p className="p-4 bg-slate-50 rounded-sm">
           I am a principle-centered, serving-leader who listens. A devoted
-          healthcare professional with over 20 years of progressive leadership and
-          management experience. I am qualified to strengthen teams, create
+          healthcare professional with over 20 years of progressive leadership
+          and management experience. I am qualified to strengthen teams, create
           growth, assure quality service, and build culture, in any healthcare
           environment. I am passionate about the care, success, and happiness of
           others and I strive to help others feel their infinite worth.{" "}
