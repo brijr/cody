c9ab597d5a73699cca48db3cec9f3c31630957d5
Bridger Tower <you@example.com>
2022-11-30 14:30:25 -0700

    Updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Libre_Baskerville } from "@next/font/google";
+
+const libre = Libre_Baskerville({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={libre.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
