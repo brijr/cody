cf02c7c79f65f42a1f2a5f132ba5b9bd0f7dab73
Bridger Tower <bridgertower@gmail.com>
2022-12-04 19:22:15 -0700

    Updated

--- components/Positive.tsx
+++ components/Positive.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Gardens from '../public/gardens.jpeg';
+import Gardens from '../public/gardens.jpg';
 import Avalon from '../public/avalon.jpg';
 import Discovery from '../public/discovery.jpg';
 
