8c2d38a28b18f0cfb1e85d45f2648f2e0fc0e888
Bridger Tower <bridgertower@gmail.com>
2022-12-05 23:42:29 -0700

    Updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
 				></link>
 				<meta
 					property="og:image"
-					content="https://cody-tower.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffam.ba2755a6.jpg&w=3840&q=75"
+					content="https://www.codytower.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffam.ba2755a6.jpg&w=3840&q=75"
 				/>
 			</Head>
 			<body>
