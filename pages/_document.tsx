4f9a3a5a0d1025f9f78f97c84ae8156aebd8f035
Bridger Tower <bridgertower@gmail.com>
2022-12-05 23:32:49 -0700

    Updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,28 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html className="bg-slate-200 scroll-smooth">
-      <Head>
-        <link href='https://css.gg/link.css' rel='stylesheet'></link>
-        <link href='https://css.gg/mail.css' rel='stylesheet'></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html className="bg-slate-200 scroll-smooth">
+			<Head>
+				<link href="https://css.gg/link.css" rel="stylesheet"></link>
+				<link href="https://css.gg/mail.css" rel="stylesheet"></link>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
+					rel="stylesheet"
+				></link>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap"
+					rel="stylesheet"
+				></link>
+				<meta
+					property="og:image"
+					content="https://cody-tower.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffam.ba2755a6.jpg&w=3840&q=75"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
