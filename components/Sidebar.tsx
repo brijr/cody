e62f20afbe745f0bc849ceb8ffb09b36af5b9b06
Bridger Tower <bridgertower@gmail.com>
2022-12-05 23:29:13 -0700

    Updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -46,13 +46,13 @@ export default function Sidebar() {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="2"
+								strokeWidth="2"
 								stroke="currentColor"
 								className="w-4 h-4"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
 								/>
 							</svg>
@@ -71,13 +71,13 @@ export default function Sidebar() {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="2"
+								strokeWidth="2"
 								stroke="currentColor"
 								className="w-4 h-4"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
 								/>
 							</svg>
@@ -96,13 +96,13 @@ export default function Sidebar() {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke-width="2"
+								strokeWidth="2"
 								stroke="currentColor"
 								className="w-4 h-4"
 							>
 								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 									d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
 								/>
 							</svg>
